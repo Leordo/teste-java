66e04ab1f5838d3f85a757a52a5b881dc469cff5
Leonardo da Rosa <68555898+Leordo@users.noreply.github.com>
2023-07-13 14:45:12 -0300

    Add files via upload
    Teste 1

---
+++ bytebankherdadoconta/ControleBonificacao.java
@@ -0,0 +1,10 @@
+package bytebankherdadoconta;
+
+public class ControleBonificacao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ bytebankherdadoconta/TesteReferencias.java
@@ -0,0 +1,12 @@
+package bytebankherdadoconta;
+
+public class TesteReferencias {
+	
+    public static void main(String[] args) {
+
+    	  Funcionario nico = new Gerente();
+
+    }
+
+	
+}
